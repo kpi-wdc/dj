a84956be93a9ef3065a3b72f9d05fe111172e848
batytskyy <victor.batyt@gmail.com>
2014-10-11 14:14:18 +0300

    initial commit

---
+++ src/main/java/org/wdc/web/controller/ViewsController.java
@@ -0,0 +1,18 @@
+package org.wdc.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/home")
+public class ViewsController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String prepare(ModelMap model) {
+        model.addAttribute("home", "page");
+        model.addAttribute("it", "is");
+        return "home";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,12 @@
+<%@ page session="false" %>
+<html>
+<head>
+    <title>My HTML View</title>
+</head>
+<body>
+<div class="success">
+    <h3>home: "${home}"</h3>
+    <h3>it: "${it}"</h3>
+</div>
+</body>
+</html>
