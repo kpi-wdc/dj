847d8cc8831ae5e7a8e14a1738855a5609501f6c
Oleksandr Sochka <sasha.sochka@gmail.com>
2014-10-12 12:26:44 +0300

    Use less instead of css, minify targeted css files

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ You need:
 To run this project from terminal run the following command:
 
      mvn compile tomcat7:run-war
+
+Suggested intellij idea plugins:
+
+  - maven
+  - spring
+  - LESS
+  - gitignore (for syntax highlight)
+  - angularjs
--- src/main/webapp/gulpfile.js
+++ src/main/webapp/gulpfile.js
@@ -1,12 +1,22 @@
 var gulp = require('gulp');
 var bower = require('gulp-bower');
+var less = require('gulp-less');
+var minifyCSS = require('gulp-minify-css');
 var del = require('del');
+var path = require('path');
 
-gulp.task('default', ['bower']);
+gulp.task('default', ['bower', 'less']);
 
 gulp.task('bower', function () {
     // bower({directory: './components', cwd: './' });
     bower('./components');
+});
+
+gulp.task('less', function () {
+    gulp.src('./WEB-INF/less/**/*.less')
+        .pipe(less())
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('./generated-css'));
 });
 
 gulp.task('clean', function (cb) {
