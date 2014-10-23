525154fb8d4b036e7087ff02c4f96fbcc6d58dcd
Oleksandr Sochka <sasha.sochka@gmail.com>
2014-10-23 23:18:52 +0300

    Add summator widget configuration (default values)

--- src/main/webapp/WEB-INF/widgets/summator/widget.js
+++ src/main/webapp/WEB-INF/widgets/summator/widget.js
@@ -1,8 +1,8 @@
 define(['angular'], function (angular) {
     angular.module('app.widgets.summator', [])
         .controller('SummatorWidgetController', function ($scope) {
-            $scope.a = 3;
-            $scope.b = 5;
+            $scope.a = $scope.widget.a || 3;
+            $scope.b = $scope.widget.b || 5;
             $scope.sum = function () {
                 return parseInt($scope.a) + parseInt($scope.b);
             }
