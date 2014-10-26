d3234508f9059ff08e969ea32cf8d188a9d663ef
batytskyy <victor.batyt@gmail.com>
2014-10-26 21:24:47 +0200

    dummy implementations of Artefact and MetadataKey Daos

--- src/main/java/org/wdc/dao/metadata/impl/ArtefactDaoImpl.java
+++ src/main/java/org/wdc/dao/metadata/impl/ArtefactDaoImpl.java
@@ -1,50 +1,20 @@
 package org.wdc.dao.metadata.impl;
 
-import org.hibernate.Criteria;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Repository;
+import org.wdc.dao.impl.HibernateDao;
 import org.wdc.dao.metadata.ArtefactDao;
 import org.wdc.domain.metadata.Artefact;
 
-import javax.transaction.Transactional;
-import java.util.List;
-
 @Repository("artefact")
-public class ArtefactDaoImpl implements ArtefactDao {
-    @Autowired
-    private SessionFactory sessionFactory;
-
-    @Override
-    public List<Artefact> getArtefacts() {
-        Session session = sessionFactory.openSession();
-        //session.beginTransaction();
-        Criteria criteria = session.createCriteria(Artefact.class);
-        List<Artefact> artefacts = (List<Artefact>) criteria.list();
-        //session.getTransaction().commit();
-
-/*        List<Artefact> artefacts = sessionFactory.getCurrentSession()
-                    .createCriteria(Artefact.class).list();*/
-
-        return artefacts;
-    }
-
-    @Override
-    public void add(Artefact artefact) {
-        Session session = sessionFactory.openSession();
-        session.save(artefact);
-    }
-
+public class ArtefactDaoImpl extends HibernateDao<Artefact, Integer>
+                             implements ArtefactDao {
     public static void main(String[] args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("spring/persistence-config.xml");
 
-        ArtefactDaoImpl impl = (ArtefactDaoImpl) ctx.getBean("artefact");
+        ArtefactDao artefactDao = (ArtefactDao) ctx.getBean("artefact");
 
-        //impl.add(new Artefact("lol", "lol2"));
-        System.out.println(impl.getArtefacts().get(0).getAssociations());
-        //impl.getArtefact();
+        System.out.println(artefactDao.list());
     }
 }
--- src/main/java/org/wdc/dao/metadata/impl/MetadataKeyDaoImpl.java
+++ src/main/java/org/wdc/dao/metadata/impl/MetadataKeyDaoImpl.java
@@ -7,32 +7,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Repository;
+import org.wdc.dao.impl.HibernateDao;
+import org.wdc.dao.metadata.ArtefactDao;
 import org.wdc.dao.metadata.MetadataKeyDao;
+import org.wdc.domain.metadata.Artefact;
 import org.wdc.domain.metadata.MetadataKey;
 
 import java.util.List;
 
 @Repository("metadatakey")
-public class MetadataKeyDaoImpl implements MetadataKeyDao {
-    @Autowired
-    private SessionFactory sessionFactory;
-
-    @Override
-    public List<MetadataKey> getMetadataKeys() {
-        Session session = sessionFactory.openSession();
-        //session.beginTransaction();
-        Criteria criteria = session.createCriteria(MetadataKey.class);
-        List<MetadataKey> metadataKeys = (List<MetadataKey>) criteria.list();
-
-        return metadataKeys;
-    }
-
+public class MetadataKeyDaoImpl extends HibernateDao<MetadataKey, Integer>
+                                implements MetadataKeyDao {
     public static void main(String[] args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("spring/persistence-config.xml");
 
-        MetadataKeyDaoImpl impl = (MetadataKeyDaoImpl) ctx.getBean("metadatakey");
+        MetadataKeyDao metadataKeyDao = (MetadataKeyDao) ctx.getBean("metadatakey");
 
-        System.out.println(impl.getMetadataKeys());
-        //impl.getArtefact();
+        System.out.println(metadataKeyDao.list());
     }
 }
