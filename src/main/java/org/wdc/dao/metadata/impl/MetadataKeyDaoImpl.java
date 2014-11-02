22a42d07bf9467faa06971824e45e2615115a8f3
batytskyy <victor.batyt@gmail.com>
2014-11-02 22:35:28 +0200

    optimise imports

--- src/main/java/org/wdc/dao/metadata/impl/MetadataKeyDaoImpl.java
+++ src/main/java/org/wdc/dao/metadata/impl/MetadataKeyDaoImpl.java
@@ -1,19 +1,11 @@
 package org.wdc.dao.metadata.impl;
 
-import org.hibernate.Criteria;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Repository;
 import org.wdc.dao.impl.HibernateDao;
-import org.wdc.dao.metadata.ArtefactDao;
 import org.wdc.dao.metadata.MetadataKeyDao;
-import org.wdc.domain.metadata.Artefact;
 import org.wdc.domain.metadata.MetadataKey;
-
-import java.util.List;
 
 @Repository("metadatakeyDao")
 public class MetadataKeyDaoImpl extends HibernateDao<MetadataKey, Integer>
