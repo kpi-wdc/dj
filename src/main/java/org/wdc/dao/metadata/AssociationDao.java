e70a8b13a5b873e84e241bff8cfe69baefa2a11c
batytskyy <victor.batyt@gmail.com>
2014-11-08 21:00:12 +0200

    add AssociationDao

---
+++ src/main/java/org/wdc/dao/metadata/AssociationDao.java
@@ -0,0 +1,8 @@
+package org.wdc.dao.metadata;
+
+import org.wdc.dao.GenericDao;
+import org.wdc.domain.metadata.Association;
+import org.wdc.domain.metadata.ids.AssociationId;
+
+public interface AssociationDao extends GenericDao<Association, AssociationId> {
+}
