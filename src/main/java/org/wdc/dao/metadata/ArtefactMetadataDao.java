f198c606a416fc28d51320c10f6cc467e5d20506
batytskyy <victor.batyt@gmail.com>
2014-11-08 20:57:03 +0200

    add ArtefactMetadataDao

---
+++ src/main/java/org/wdc/dao/metadata/ArtefactMetadataDao.java
@@ -0,0 +1,9 @@
+package org.wdc.dao.metadata;
+
+import org.wdc.dao.GenericDao;
+import org.wdc.domain.metadata.ArtefactMetadata;
+import org.wdc.domain.metadata.ids.ArtefactMetadataId;
+
+public interface ArtefactMetadataDao
+        extends GenericDao<ArtefactMetadata, ArtefactMetadataId> {
+}
