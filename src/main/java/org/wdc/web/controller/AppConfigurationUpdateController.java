f38ba83a9890e0b8ee30e73933d998dcede05ebb
batytskyy <victor.batyt@gmail.com>
2014-11-09 19:23:42 +0200

    [controller] use FileSystemResourse instead of InputStreamResource

---
+++ src/main/java/org/wdc/web/controller/AppConfigurationUpdateController.java
@@ -0,0 +1,5 @@
+package org.wdc.web.controller;
+
+public class AppConfigurationUpdateController {
+
+}
--- src/main/java/org/wdc/web/controller/ViewsController.java
+++ src/main/java/org/wdc/web/controller/ViewsController.java
@@ -1,27 +1,19 @@
 package org.wdc.web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.ServletContext;
-import java.io.InputStream;
-
 @Controller
 @RequestMapping("/")
 public class ViewsController {
-    @Autowired
-    private ServletContext servletContext;
-
     @RequestMapping(method = RequestMethod.GET, produces = "text/html")
-    public ResponseEntity<InputStreamResource> home() {
-
-        InputStream in = servletContext.getResourceAsStream("/build/index.html");
-        InputStreamResource inputStreamResource = new InputStreamResource(in);
-        return new ResponseEntity<>(inputStreamResource, HttpStatus.OK);
+    public ResponseEntity<FileSystemResource> home() {
+        FileSystemResource fileSystemResource =
+                new FileSystemResource("src/main/webapp/build/index.html");
+        return new ResponseEntity<>(fileSystemResource, HttpStatus.OK);
     }
 }
