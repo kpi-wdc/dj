0e1ede6795370d9c4641e51ecc4c98e8df7de908
batytskyy <victor.batyt@gmail.com>
2014-11-09 16:13:33 +0200

    [service] add MetadataKeyDefService

--- src/main/java/org/wdc/service/MetadataKeyDefService.java
+++ src/main/java/org/wdc/service/MetadataKeyDefService.java
@@ -1,5 +1,13 @@
 package org.wdc.service;
 
-public interface MetadataKeyDefService {
+import org.wdc.domain.metadata.MetadataKeyDef;
 
+import java.util.List;
+
+public interface MetadataKeyDefService {
+    public void addMetadataKeyDef(String metadataKey, String defKey, String value);
+    public void updateMetadataKeyDef(String metadataKey, String defKey, String newValue);
+    public void removeMetadataKeyDef(String metadataKey, String defKey);
+    public MetadataKeyDef findByMetadataKeyAndDefKeys(String metadataKey, String defKey);
+    public List<MetadataKeyDef> list();
 }
--- src/main/java/org/wdc/service/impl/MetadataKeyDefServiceImpl.java
+++ src/main/java/org/wdc/service/impl/MetadataKeyDefServiceImpl.java
@@ -1,5 +1,77 @@
 package org.wdc.service.impl;
 
-public class MetadataKeyDefServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.wdc.dao.metadata.MetadataKeyDao;
+import org.wdc.dao.metadata.MetadataKeyDefDao;
+import org.wdc.domain.metadata.MetadataKey;
+import org.wdc.domain.metadata.MetadataKeyDef;
+import org.wdc.domain.metadata.ids.MetadataKeyDefId;
+import org.wdc.service.MetadataKeyDefService;
 
+import java.util.List;
+
+@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
+@Service("metadataKeyDefService")
+public class MetadataKeyDefServiceImpl implements MetadataKeyDefService {
+    @Autowired
+    private MetadataKeyDefDao metadataKeyDefDao;
+
+    @Autowired
+    private MetadataKeyDao metadataKeyDao;
+
+    public MetadataKeyDefDao getMetadataKeyDefDao() {
+        return metadataKeyDefDao;
+    }
+
+    public void setMetadataKeyDefDao(MetadataKeyDefDao metadataKeyDefDao) {
+        this.metadataKeyDefDao = metadataKeyDefDao;
+    }
+
+    public MetadataKeyDao getMetadataKeyDao() {
+        return metadataKeyDao;
+    }
+
+    public void setMetadataKeyDao(MetadataKeyDao metadataKeyDao) {
+        this.metadataKeyDao = metadataKeyDao;
+    }
+
+    @Override
+    public void addMetadataKeyDef(String metadataKey, String defKey, String value) {
+        MetadataKey mdataKey = metadataKeyDao.findByKey(metadataKey);
+        MetadataKey def = metadataKeyDao.findByKey(defKey);
+
+        metadataKeyDefDao.add(new MetadataKeyDef(new MetadataKeyDefId(mdataKey, def), value));
+    }
+
+    @Override
+    public void updateMetadataKeyDef(String metadataKey, String defKey, String newValue) {
+        MetadataKey mdataKey = metadataKeyDao.findByKey(metadataKey);
+        MetadataKey def = metadataKeyDao.findByKey(defKey);
+
+        metadataKeyDefDao.update(new MetadataKeyDef(new MetadataKeyDefId(mdataKey, def), newValue));
+    }
+
+    @Override
+    public void removeMetadataKeyDef(String metadataKey, String defKey) {
+        MetadataKey mdataKey = metadataKeyDao.findByKey(metadataKey);
+        MetadataKey def = metadataKeyDao.findByKey(defKey);
+
+        metadataKeyDefDao.remove(new MetadataKeyDef(new MetadataKeyDefId(mdataKey, def)));
+    }
+
+    @Override
+    public MetadataKeyDef findByMetadataKeyAndDefKeys(String metadataKey, String defKey) {
+        MetadataKey mdataKey = metadataKeyDao.findByKey(metadataKey);
+        MetadataKey def = metadataKeyDao.findByKey(defKey);
+
+        return metadataKeyDefDao.find(new MetadataKeyDefId(mdataKey, def));
+    }
+
+    @Override
+    public List<MetadataKeyDef> list() {
+        return metadataKeyDefDao.list();
+    }
 }
