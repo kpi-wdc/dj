e133c6157f8f43a6448777bda618744635106a15
batytskyy <victor.batyt@gmail.com>
2014-11-09 20:31:43 +0200

    [contorller] save app.json file in pretty format

--- src/main/java/org/wdc/web/controller/AppConfigurationUpdateController.java
+++ src/main/java/org/wdc/web/controller/AppConfigurationUpdateController.java
@@ -1,10 +1,14 @@
 package org.wdc.web.controller;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @Controller
 @RequestMapping("/apps")
@@ -20,6 +24,15 @@ public class AppConfigurationUpdateController {
     @ResponseBody
     @RequestMapping(value = "/app.json", method = RequestMethod.PUT)
     public void updateConfiguration(@RequestBody String body) {
-        System.out.println(body);
+        try (PrintWriter out = new PrintWriter("src/main/webapp/resources/apps/app.json")) {
+            // convert to pretty view of the json
+            ObjectMapper mapper = new ObjectMapper();
+            Object json = mapper.readValue(body, Object.class);
+            String prettyBody = mapper.writerWithDefaultPrettyPrinter().
+                    writeValueAsString(json);
+            out.print(prettyBody);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
