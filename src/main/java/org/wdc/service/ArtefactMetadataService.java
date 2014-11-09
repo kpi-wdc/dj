66c39c7183a304957e79f55533b41328957db14a
batytskyy <victor.batyt@gmail.com>
2014-11-09 16:43:53 +0200

    [service] add ArtefactMetadataService

--- src/main/java/org/wdc/service/ArtefactMetadataService.java
+++ src/main/java/org/wdc/service/ArtefactMetadataService.java
@@ -1,5 +1,15 @@
 package org.wdc.service;
 
-public interface ArtefactMetadataService {
+import org.wdc.domain.metadata.ArtefactMetadata;
 
+import java.util.List;
+
+public interface ArtefactMetadataService {
+    public void addArtefactMetadata(String artefactKey, String metadataKey, String value);
+    public void updateArtefactMetadata(String artefactKey,
+                                       String metadataKey, String newValue);
+    public void removeArtefactMetadata(String artefactKey, String metadataKey);
+    public ArtefactMetadata findByArtefactAndMetadataKeys(String artefactKey,
+                                                          String metadataKey);
+    public List<ArtefactMetadata> list();
 }
