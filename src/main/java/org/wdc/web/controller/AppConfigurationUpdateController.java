913d3091180a95e87b28e0bbd1a588893787a3e9
batytskyy <victor.batyt@gmail.com>
2014-11-09 19:25:03 +0200

    [controller] handle all get request to /apps

--- src/main/java/org/wdc/web/controller/AppConfigurationUpdateController.java
+++ src/main/java/org/wdc/web/controller/AppConfigurationUpdateController.java
@@ -1,5 +1,19 @@
 package org.wdc.web.controller;
 
-public class AppConfigurationUpdateController {
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+@Controller
+@RequestMapping("/apps")
+public class AppConfigurationUpdateController {
+    @RequestMapping(value="{name}", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public ResponseEntity<FileSystemResource> getJSON(@PathVariable String name) {
+        FileSystemResource fileSystemResource =
+                new FileSystemResource("src/main/webapp/resources/apps/" + name + ".json");
+        return new ResponseEntity<>(fileSystemResource, HttpStatus.OK);
+    }
 }
