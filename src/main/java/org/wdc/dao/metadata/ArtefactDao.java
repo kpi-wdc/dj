892c502603b3bf3f5e094f966d134c6e959a87f9
batytskyy <victor.batyt@gmail.com>
2014-11-09 17:08:14 +0200

    [hibernate] refactor imports

--- src/main/java/org/wdc/dao/metadata/ArtefactDao.java
+++ src/main/java/org/wdc/dao/metadata/ArtefactDao.java
@@ -3,8 +3,6 @@ package org.wdc.dao.metadata;
 import org.wdc.dao.GenericDao;
 import org.wdc.domain.metadata.Artefact;
 
-import java.util.List;
-
 public interface ArtefactDao extends GenericDao<Artefact, Integer> {
     public Artefact findByKey(String key);
 }
