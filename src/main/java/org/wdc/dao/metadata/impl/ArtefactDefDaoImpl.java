b638a8784830e870614e95520390349e62374aca
batytskyy <victor.batyt@gmail.com>
2014-11-09 17:07:10 +0200

    [hibernate] refactor imports

--- src/main/java/org/wdc/dao/metadata/impl/ArtefactDefDaoImpl.java
+++ src/main/java/org/wdc/dao/metadata/impl/ArtefactDefDaoImpl.java
@@ -1,19 +1,9 @@
 package org.wdc.dao.metadata.impl;
 
-import org.hibernate.Hibernate;
-import org.hibernate.criterion.Restrictions;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import org.wdc.dao.impl.HibernateDao;
-import org.wdc.dao.metadata.ArtefactDao;
 import org.wdc.dao.metadata.ArtefactDefDao;
-import org.wdc.dao.metadata.MetadataKeyDao;
-import org.wdc.domain.metadata.Artefact;
 import org.wdc.domain.metadata.ArtefactDef;
-import org.wdc.domain.metadata.MetadataKey;
 import org.wdc.domain.metadata.ids.ArtefactDefId;
 
 @Repository("artefactDefDao")
