4b5816d17a78afaf6e01988e551dcf702abeed29
Oleksandr Sochka <sasha.sochka@gmail.com>
2014-11-14 19:59:46 +0200

    [front-end] remove redundant require config in leaflet-map

--- src/main/webapp/resources/widgets/leaflet-map/widget.js
+++ src/main/webapp/resources/widgets/leaflet-map/widget.js
@@ -1,18 +1,3 @@
-require.config({
-    paths: {
-        'leaflet': '/components/leaflet/dist/leaflet',
-        'angular-leaflet': '/components/angular-leaflet/dist/angular-leaflet-directive'
-    },
-    shim: {
-        'leaflet': {
-            exports: 'L'
-        },
-        'angular-leaflet': {
-            deps: ['leaflet']
-        }
-    }
-});
-
 define(['angular', 'angular-leaflet'], function (angular) {
     angular.module('app.widgets.leaflet-map', ['leaflet-directive', {files: ['/components/leaflet/dist/leaflet.css']}])
 //        .controller('LeafletMapController', function ($scope) {
