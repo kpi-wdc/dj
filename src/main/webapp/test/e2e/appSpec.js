b62128c4ad910412a5135c2edf924211961a3e29
Oleksandr Sochka <sasha.sochka@gmail.com>
2014-11-15 01:44:40 +0200

    [travis, protractor] Make travis work with protractor (buggy)

--- src/main/webapp/test/e2e/appSpec.js
+++ src/main/webapp/test/e2e/appSpec.js
@@ -1,6 +1,9 @@
 describe("End-to-end tests", function () {
     it('Home page test', function() {
         browser.get('/');
+        $('body').getText().then(function (text) {
+            console.dir(text);
+        });
         expect($('.page-title').getText()).toBe("Home page");
     });
 });
