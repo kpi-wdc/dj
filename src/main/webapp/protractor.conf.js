99f282d1ec67fda4812092127d3ce88424e48597
Oleksandr Sochka <sasha.sochka@gmail.com>
2014-11-15 13:44:14 +0200

    [protractor] run selenium server automatically

--- src/main/webapp/protractor.conf.js
+++ src/main/webapp/protractor.conf.js
@@ -1,5 +1,4 @@
 exports.config = {
-    seleniumAddress: 'http://localhost:4444/wd/hub',
     baseUrl: 'http://localhost:8080',
     specs: ['build/test/e2e/**/*Spec.js'],
     allScriptsTimeout: 10000,
