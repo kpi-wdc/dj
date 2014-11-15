6b8543f54887d87b0bfebb61ef65879c5b89f194
Oleksandr Sochka <sasha.sochka@gmail.com>
2014-11-15 20:03:45 +0200

    [e2e] refactor appSpec

--- src/main/webapp/test/e2e/appSpec.js
+++ src/main/webapp/test/e2e/appSpec.js
@@ -1,12 +1,15 @@
-describe("Home page", function () {
+describe("Webapp", function () {
     beforeEach(function () {
         browser.get('/');
     });
 
-    it('Home page test', function() {
-        $('body').getText().then(function (text) {
-            console.dir(text);
+    it('should have some content', function () {
+        $('html').getText().then(function (text) {
+            console.dir(text); // log for debugging on Travis CI
         });
+    });
+
+    it('should have home page title', function() {
         expect($('.page-title').getText()).toBe("Home page");
     });
 });
