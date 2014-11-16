cc8dcc43b73ee0734d5c01ff3f47ff4618c1bb8a
batytskyy <victor.batyt@gmail.com>
2014-11-16 20:35:11 +0200

    [service] add custom Exceptions support

--- src/main/java/org/wdc/service/ArtefactDefService.java
+++ src/main/java/org/wdc/service/ArtefactDefService.java
@@ -1,15 +1,23 @@
 package org.wdc.service;
 
 import org.wdc.domain.metadata.ArtefactDef;
+import org.wdc.exception.DuplicateIdException;
+import org.wdc.exception.NoIdException;
 
 import java.util.List;
 
 public interface ArtefactDefService {
     public void addArtefactDef(String artefactKey, String metadataKey,
-                               boolean required, boolean unique, String defaultValue);
+                boolean required, boolean unique, String defaultValue)
+            throws DuplicateIdException;
+
     public void updateArtefactDef(String artefactKey, String metadataKey,
-                                  boolean required, boolean unique, String defaultValue);
-    public void removeArtefactDef(String artefactKey, String metadataKey);
+                boolean required, boolean unique, String defaultValue)
+            throws NoIdException;
+
+    public void removeArtefactDef(String artefactKey, String metadataKey)
+            throws NoIdException;
+
     public ArtefactDef findByArtefactAndMetadataKeys(String artefactKey, String metadataKey);
     public List<ArtefactDef> list();
 }
--- src/main/java/org/wdc/service/impl/ArtefactDefServiceImpl.java
+++ src/main/java/org/wdc/service/impl/ArtefactDefServiceImpl.java
@@ -11,6 +11,8 @@ import org.wdc.domain.metadata.Artefact;
 import org.wdc.domain.metadata.ArtefactDef;
 import org.wdc.domain.metadata.MetadataKey;
 import org.wdc.domain.metadata.ids.ArtefactDefId;
+import org.wdc.exception.DuplicateIdException;
+import org.wdc.exception.NoIdException;
 import org.wdc.service.ArtefactDefService;
 
 import java.util.List;
@@ -52,29 +54,36 @@ public class ArtefactDefServiceImpl implements ArtefactDefService {
     }
 
     @Override
-    public void addArtefactDef(String artefactKey, String metadataKey, boolean required, boolean unique, String defaultValue) {
+    public void addArtefactDef(String artefactKey, String metadataKey, boolean required, boolean unique, String defaultValue)
+            throws DuplicateIdException {
         Artefact artefact = artefactDao.findByKey(artefactKey);
         MetadataKey mdataKey = metadataKeyDao.findByKey(metadataKey);
+        ArtefactDefId id = new ArtefactDefId(artefact, mdataKey);
 
-        artefactDefDao.add(new ArtefactDef(new ArtefactDefId(artefact, mdataKey),
-                required, unique, defaultValue));
+        if (hasArtefactDef(id)) throw new DuplicateIdException(id + " already exists");
+
+        artefactDefDao.add(new ArtefactDef(id, required, unique, defaultValue));
     }
 
     @Override
-    public void updateArtefactDef(String artefactKey, String metadataKey, boolean required, boolean unique, String defaultValue) {
+    public void updateArtefactDef(String artefactKey, String metadataKey, boolean required,
+                                  boolean unique, String defaultValue) throws NoIdException {
         Artefact artefact = artefactDao.findByKey(artefactKey);
         MetadataKey mdataKey = metadataKeyDao.findByKey(metadataKey);
-
-        artefactDefDao.update(new ArtefactDef(new ArtefactDefId(artefact, mdataKey),
-                required, unique, defaultValue));
+        ArtefactDefId id = new ArtefactDefId(artefact, mdataKey);
+        if (!hasArtefactDef(id)) throw new NoIdException(id + " doesn't exist");
+        artefactDefDao.update(new ArtefactDef(id, required, unique, defaultValue));
     }
 
     @Override
-    public void removeArtefactDef(String artefactKey, String metadataKey) {
+    public void removeArtefactDef(String artefactKey, String metadataKey) throws NoIdException {
         Artefact artefact = artefactDao.findByKey(artefactKey);
         MetadataKey mdataKey = metadataKeyDao.findByKey(metadataKey);
 
-        artefactDefDao.remove(new ArtefactDef(new ArtefactDefId(artefact, mdataKey)));
+        ArtefactDefId id = new ArtefactDefId(artefact, mdataKey);
+        if (!hasArtefactDef(id)) throw new NoIdException(id + " doesn't exist");
+
+        artefactDefDao.remove(new ArtefactDef(id));
     }
 
     @Override
@@ -88,5 +97,10 @@ public class ArtefactDefServiceImpl implements ArtefactDefService {
     @Override
     public List<ArtefactDef> list() {
         return artefactDefDao.list();
+    }
+
+    private boolean hasArtefactDef(ArtefactDefId id) {
+        ArtefactDef def = artefactDefDao.find(id);
+        return def != null;
     }
 }
