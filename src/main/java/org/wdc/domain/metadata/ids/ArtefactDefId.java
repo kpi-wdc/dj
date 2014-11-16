8cb8f69bf123b15d40c689e83f6e85eaa7a1b0f9
batytskyy <victor.batyt@gmail.com>
2014-11-16 20:10:36 +0200

    [hibernate] add toString() method

--- src/main/java/org/wdc/domain/metadata/ids/ArtefactDefId.java
+++ src/main/java/org/wdc/domain/metadata/ids/ArtefactDefId.java
@@ -63,4 +63,12 @@ public class ArtefactDefId implements Serializable {
         result = 31 * result + (metadataKey != null ? metadataKey.hashCode() : 0);
         return result;
     }
+
+    @Override
+    public String toString() {
+        return "ArtefactDefId{" +
+                "artefact.key=" + artefact.getKey() +
+                ", metadataKey.key=" + metadataKey.getKey() +
+                '}';
+    }
 }
