1bd32802e950aaace5298e3ebcae9fef3da5df60
Oleksandr Sochka <sasha.sochka@gmail.com>
2014-11-23 21:47:24 +0200

    [front-end] make HtmlWidget reconfigurable

--- src/main/webapp/resources/widgets/htmlwidget/widget.js
+++ src/main/webapp/resources/widgets/htmlwidget/widget.js
@@ -1,6 +1,9 @@
 define(['angular'], function (angular) {
     angular.module('app.widgets.htmlwidget', [])
-        .controller('HtmlWidgetController', function ($scope, $sce) {
-            $scope.text = $sce.trustAsHtml($scope.widget.text);
+        .controller('HtmlWidgetController', function ($scope, $sce, APIProvider) {
+            new APIProvider($scope)
+                .config(function () {
+                    $scope.text = $sce.trustAsHtml($scope.widget.text);
+                });
         });
 });
