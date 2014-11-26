2e1db5daa79de5a9802e17e57da5c288e7b934b4
batytskyy <victor.batyt@gmail.com>
2014-11-26 21:49:03 +0200

    [controller] delete unused import

--- src/main/java/org/wdc/web/controller/WidgetUpdateController.java
+++ src/main/java/org/wdc/web/controller/WidgetUpdateController.java
@@ -11,7 +11,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
 import java.util.LinkedHashMap;
-import java.util.Scanner;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
