046bc330f60a993b2030264a5199f0bc341fda63
Oleksandr Sochka <sasha.sochka@gmail.com>
2014-12-02 22:48:03 +0200

    [front-end] fix e2e tests

--- src/main/webapp/test/e2e/appSpec.js
+++ src/main/webapp/test/e2e/appSpec.js
@@ -8,8 +8,17 @@ describe("Webapp", function () {
             });
         }, 3000).then(function () {
             browser.ignoreSynchronization = false;
+            $('#logInButton').click();
+            $('#designModeCheckbox').click();
             cb();
         });
+    });
+
+    afterEach(function (cb) {
+        browser.executeScript(function () {
+            window.localStorage.clear();
+        });
+        cb();
     });
 
     it('should have some content', function () {
