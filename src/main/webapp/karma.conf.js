526990de7f38710250a613a64f11ecc29d50dc29
Oleksandr Sochka <sasha.sochka@gmail.com>
2014-12-12 20:18:47 +0200

    [karma, unit-test] fix unit-tests (load es6 polyfill in karma)

--- src/main/webapp/karma.conf.js
+++ src/main/webapp/karma.conf.js
@@ -4,16 +4,23 @@ module.exports = function(config){
     basePath : './build',
 
     files : [
-      'js/main.js',
-      {pattern: 'js/*.js', included: false},
-      {pattern: 'widgets/**/*.js', included: false},
-      {pattern: 'components/**/*.js', included: false},
-      {pattern: 'test/unit/**/*Spec.js', included: false}
+        'js/browser-polyfill.js',
+        // HACK: manually inject RequireJS framework
+        // otherwise it's impossible to load polyfills before other code
+        // Note: RequireJS is not listed in `frameworks` property
+        'components/requirejs/require.js',
+        '../node_modules/karma-requirejs/lib/adapter.js',
+        // END OF HACK
+        'js/main.js',
+        {pattern: 'js/*.js', included: false},
+        {pattern: 'widgets/**/*.js', included: false},
+        {pattern: 'components/**/*.js', included: false},
+        {pattern: 'test/unit/**/*Spec.js', included: false}
     ],
 
     autoWatch : true,
 
-    frameworks: ['jasmine', 'requirejs'],
+    frameworks: ['jasmine'],
 
     browsers : ['PhantomJS'],
 
