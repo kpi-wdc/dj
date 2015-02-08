9ae9a7b1f66be357bba5bdc61fbaa2dfc52e5257
Oleksandr Sochka <sasha.sochka@gmail.com>
2015-02-08 23:19:47 +0200

    [db] config mongodb connection for models

--- config/connections.js
+++ config/connections.js
@@ -60,6 +60,7 @@ module.exports.connections = {
     adapter: 'sails-mongo',
     host: 'localhost',
     port: 27017,
+    url: process.env.MONGOLAB_URI,
     // user: 'username',
     // password: 'password',
     // database: 'your_mongo_db_name_here'
