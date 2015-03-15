b65b475beeffb30996cb82b0ee696822ddfc800b
Oleksandr Sochka <sasha.sochka@gmail.com>
2015-03-15 21:26:19 +0200

    [back-end] create default app, cloned when creating the new one

--- api/controllers/AppController.js
+++ api/controllers/AppController.js
@@ -9,21 +9,27 @@ module.exports = {
   _config: { actions: true, rest: false, shortcuts: false },
 
   create: function (req, res) {
-    AppConfig.create({
-      name: req.params.appName,
-      pages: [ { "shortTitle" : "Home", "href": "", "template" : "1-col", "holders" : { "column": { "widgets": [ { "type": "title", "title": "Home page", "instanceName": "title-widget"}, { "type": "htmlwidget", "instanceName": "main-page-html-widget", "text": "<h3>Page Title <small>Page subtitle</small></h3>Bacon ipsum dolor sit amet salami ham."}]}}}, { "href": "404", "template" : "1-col", "holders" : { "column": { "widgets": [ { "type": "title", "title": "404 error", "instanceName": "title"}, { "type": "htmlwidget", "text": "Page not found", "instanceName": "error-message"}]}}}],
-      title: "Title",
-      description: "Description",
-      isPublished: true,
-      owner: req.user.id
-    }, function (err) {
-      if (err) {
-        sails.log.error('Error while creating app: ' + err);
+    // Clone default application
+    AppConfig.findOne({name: 'default'})
+      .then(function (newApp) {
+        delete newApp.id;
+        newApp.isPublished = true;
+        newApp.name = req.params.appName
+        newApp.owner = req.user.id;
+
+        AppConfig.create(newApp, function (err) {
+          if (err) {
+            sails.log.error('Error while creating app: ' + err);
+            res.serverError();
+          } else {
+            res.ok();
+          }
+        });
+      })
+      .catch(function (error) {
+        sails.log.warn('Error in AppController.create: ' + error);
         res.serverError();
-      } else {
-        res.ok();
-      }
-    });
+      });
   },
 
   update: function (req, res) {
