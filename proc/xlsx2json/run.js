7f9f149f084d8879828570df9733ceeaef96c2f8
batytskyy <victor.batyt@gmail.com>
2015-03-22 22:01:16 +0200

    [node] update module to return metadata and data separately

--- proc/xlsx2json/run.js
+++ proc/xlsx2json/run.js
@@ -3,4 +3,24 @@ var xlsxJsonParser = require('./index');
 var args = process.argv.slice(2);
 var json = xlsxJsonParser.get(args[0]);
 
-process.stdout.write(json);
+// get name of the datasource
+var dataSourceName;
+for (dataSourceName in json) {
+    if (json.hasOwnProperty(dataSourceName)) {
+        break;
+    }
+}
+console.log(dataSourceName);
+
+var result = {};
+result.name = dataSourceName;
+result.metadata = {};
+for(var prop in json[dataSourceName]) {
+    if (prop !== "value") {
+        result.metadata[prop] = json[dataSourceName][prop];
+    } else {
+        result.value = json[dataSourceName]["value"];
+    }
+}
+
+process.stdout.write(JSON.stringify(result));
