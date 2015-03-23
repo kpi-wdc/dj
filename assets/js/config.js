3905746d022f349402a92ed6b6c650fc42761e3f
Andrey Boldak <boldak.andrey@gmail.com>
2015-03-23 12:04:09 +0200

    merge with develop

--- assets/js/config.js
+++ assets/js/config.js
@@ -25,7 +25,7 @@ System.config({
     "json-stat": "components/jsonstat/json-stat.max.js",
     "nv.d3": "components/nvd3/nv.d3.js",
     "*": "*.js",
-    "github:*": "../../jspm_packages/github/*.js"
+    "github:*": "..\\..\\jspm_packages/github/*.js"
   }
 });
 
