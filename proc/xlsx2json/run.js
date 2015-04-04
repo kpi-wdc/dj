9ea8672dda3a4c859d4cf21572a6c271596a7a17
Serhii Podvysotskyi <Serhii.podvysotskyi@gmail.com>
2015-04-04 18:35:31 +0300

    using function getOBJECT

--- proc/xlsx2json/run.js
+++ proc/xlsx2json/run.js
@@ -2,26 +2,9 @@ var xlsxJsonParser = require('./index');
 
 var args = process.argv.slice(2);
 
-var json = JSON.parse(xlsxJsonParser.readJSON(args[0]));
+var json = xlsxJsonParser.readJSON(args[0]);
 
-// get name of the datasource
-var dataSourceName;
-for (dataSourceName in json) {
-    if (json.hasOwnProperty(dataSourceName)) {
-        break;
-    }
-}
-
-var result = {};
-result.name = dataSourceName;
-result.metadata = {};
-for(var prop in json[dataSourceName]) {
-    if (prop !== "value") {
-        result.metadata[prop] = json[dataSourceName][prop];
-    } else {
-        result.value = json[dataSourceName]["value"];
-    }
-}
+var result = xlsxJsonParser.getOBJECT(json);
 
 process.send(result);
 process.exit(0);
