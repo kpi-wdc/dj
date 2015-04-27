ea829430bfc12ed51143623ad45d50039a180d40
Andrey Boldak <boldak.andrey@gmail.com>
2015-04-27 13:12:45 +0300

    [front-end] fix isString in TableGenerator adapter

--- assets/widgets/data-util/adapter.js
+++ assets/widgets/data-util/adapter.js
@@ -11,11 +11,10 @@ System.config({
 define(["angular", "jsinq", "jsinq-query", "stat", "pca", "cluster"], function (angular, jsinq) {
   var m = angular.module("app.widgets.data-util.adapter", ["app.widgets.data-util.stat", "app.widgets.data-util.pca", "app.widgets.data-util.cluster"]);
 
-  m.service("TableGenerator", function () {
-    var str = function str(arg) {
-      return angular.isString(arg) ? "'" + arg + "'" : arg;
+  var str = function str(arg) {
+      return angular.isString(arg) ? '"' + arg + '"' : arg;
     };
-
+    
     this.getData = function (conf, provider) {
       if (angular.isUndefined(conf) && angular.isUndefined(provider)) return undefined;
 
