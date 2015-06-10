6c042feb412e660522de0cd1a46738533edcbb33
Oleksandr Sochka <sasha.sochka@gmail.com>
2015-06-11 02:55:30 +0300

    [front-end] allow design mode for non-logged users in case of non-owned apps

--- api/models/AppConfig.js
+++ api/models/AppConfig.js
@@ -74,13 +74,13 @@ module.exports = {
   },
 
   isOwner: function (app, user) {
-    if (!user) {
-      // user is not logged in - therefore not an owner
-      return false;
-    }
     if (!app.owner) {
       // No owner means every logged user is an owner
       return true;
+    }
+    if (!user) {
+      // user is not logged in - therefore not an owner
+      return false;
     }
     return app.owner.id === user.id;
   }
