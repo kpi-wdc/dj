145ae961640c64e6df0d3f1aba8583d6082aaf32
batytskyy <victor.batyt@gmail.com>
2015-11-08 21:51:19 +0200

    [data-processor] introduce cache request property

--- config/executables.js
+++ config/executables.js
@@ -1,20 +1,19 @@
 module.exports.executables = {
-  xlsx2json: './proc/xlsx2json/xlsx2json.js',
-  json2jsonstat: './proc/xlsx2json/json2jsonstat.js',
-  avg: './proc/avg/run.js',
-  multiplier: './proc/multiplier/run.js',
+  'xlsx2json'             : './proc/xlsx2json/xlsx2json.js',
+  'json2jsonstat'         : './proc/xlsx2json/json2jsonstat.js',
+  'avg'                   : './proc/avg/run.js',
+  'multiplier'            : './proc/multiplier/run.js',
   'math/vector/multiplier': './proc/multiplier/run.js',
-  update: './proc/update/run.js',
-  
-  "query"			: './proc/query/run.js',
-  "barchartserie"	: './proc/serie-adapter/bar-serie/run.js',
-  "corr-matrix"		: './proc/serie-adapter/corr-matrix/run.js',
-  "corr-table"		: './proc/serie-adapter/corr-table/run.js',
-  "map-serie"		: './proc/serie-adapter/map-serie/run.js',
-  "normalizer"		: './proc/serie-adapter/normalizer/run.js',
-  "reduce-null"		: './proc/serie-adapter/reduce-null/run.js',
-  "scatter-serie"	: './proc/serie-adapter/scatter-serie/run.js',
-  "distribution"	: './proc/serie-adapter/distribution/run.js',
-  "post-process"  : './proc/serie-adapter/post-process/run.js'
-      
+  'update'                : './proc/update/run.js',
+
+  'query'			    : './proc/query/run.js',
+  'barchartserie'	: './proc/serie-adapter/bar-serie/run.js',
+  'corr-matrix'		: './proc/serie-adapter/corr-matrix/run.js',
+  'corr-table'		: './proc/serie-adapter/corr-table/run.js',
+  'map-serie'		  : './proc/serie-adapter/map-serie/run.js',
+  'normalizer'		: './proc/serie-adapter/normalizer/run.js',
+  'reduce-null'		: './proc/serie-adapter/reduce-null/run.js',
+  'scatter-serie'	: './proc/serie-adapter/scatter-serie/run.js',
+  'distribution'	: './proc/serie-adapter/distribution/run.js',
+  'post-process'  : './proc/serie-adapter/post-process/run.js'
 };
